5029b7da1a4a253d6dc1f3a3f05ca0e36c7bd2aa
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-06-09 15:23:11 +0330

    Handling Events the React Way

--- src/App.js
+++ src/App.js
@@ -6,20 +6,39 @@ const messages = [
 
 function App() {
   const step = 1 ;
+
+  const handlePrevious = () => {
+    alert("Previous");
+  }
+  const handleNext = () => {
+    alert('Next')
+  }
+
+
+
+
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
-        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
       </div>
 
-      <p className="message">Step {step}: {messages[step - 1]}</p>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
